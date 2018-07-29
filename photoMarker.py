fe5c0857fb34f3351535d23a920ad1db032d3a1d
huklee <lhwnotss@gmail.com>
2018-07-29 13:22:24 +0900

    fix broken filename into ascii

--- photoMarker.py
+++ photoMarker.py
@@ -2,16 +2,19 @@ from geoMarker import *
 from PIL import Image
 from IPython.display import display 
 import os
+import re
+
 
 changeSet = {"카이스트" : "KAIST",
              "오션월드" : "OceanWorld",
              "문래동" : "Mullae-dong",
-             "크리스마스" : "Mullae-dong",
-             
+             "크리스마스" : "Mullae-dong",             
         }
+
 
 def decode(s):
     return s.encode('utf-8', 'surrogateescape').decode('UTF-8')
+
 
 def countName(countSet, name):
     if name not in countSet:
@@ -19,27 +22,49 @@ def countName(countSet, name):
     countSet[name] += 1
     return str(countSet[name])
 
-def changeKo2En(countSet, word):
+
+def getKo2En(w):
     suffixList = []
-    for w in changeSet:
-        if w in word:
-            suffixList.append(changeSet[w])
+    for koWord in changeSet:
+        if koWord in w:
+            suffixList.append(changeSet[koWord])
     suffix = "_".join(suffixList)
-    countNumber = countName(countSet, word[:11] + suffix)
-    return word[:11] + suffix + "_" + countNumber + word[-4:]
+    return suffix
+
+def longestEnWord(w):
+    base = os.path.basename(w)
+    fileName, ext = os.path.splitext(base)
+    p = re.compile("[a-zA-Z ]")
+    pos = ([m.start() for m in p.finditer(fileName[11:])])
+    if len(pos) < 2:
+        return ""
+    else:
+        return "".join([fileName[11 + i] for i in pos]).strip()
 
 def fixBrokenEncodingFileName(dirName):
     countSet = {}
     dList = sorted(os.listdir(dirName))
     for f in dList:
-        if not f[-4:].lower() == ".jpg":
+        _, ext = os.path.splitext(f)
+        if not ext.lower() == ".jpg":
             continue
+            
+        # convert ko into en
         fileName = decode(f)
-        newName = changeKo2En(countSet, fileName)
-        print(fileName, " ==> ", newName)
-        os.rename(os.path.join(dirName, f), os.path.join(dirName, newName))   
-    
+        ko2EnName = getKo2En(fileName)
+        enName = longestEnWord(fileName)
+        nameSet = [ko2EnName, enName]
+        if "" in nameSet: nameSet.remove("");
 
+        # put Count Number & ext
+        newName = f[:11] + "_".join(nameSet)
+        countNumber = countName(countSet, newName)
+        newFileName = "_".join([newName, countNumber]) + ext
+        
+        print(fileName, "==>", newFileName)
+        os.rename(os.path.join(dirName, f), os.path.join(dirName, newFileName))  
+
+        
 locationSet = {
     "KAIST":(36.372902, 127.360034),
     "OceanWorld":(37.648157, 127.685571),
@@ -72,16 +97,22 @@ def markAllGeoDateDir(dirName, newdirName=""):
         lat, lon = getLocationSet(f)
         modiExif(filePath, newPath, lat, lon, getDate(f))
     
-def displayAllPictures(dirName):
-    dList = sorted(os.listdir(dirName))
+def displayAllPictures(dirName, maxCount=99999):
+    dList = sorted(os.listdir(dirName), key=lambda x:decode(x))
+    count = 0
     for f in dList:
         if f[-4:].lower() != ".jpg":
             continue
 
+        if count < maxCount:
+            count += 1
+        else:
+            break
+            
         filePath = os.path.join(dirName, f)        
         pil_im = Image.open(filePath, 'r')
         display(pil_im)
-        print(f)
+        print(decode(filePath))
 
 # displayAllPictures("2008")
 # markAllGeoDateDir("2008", "_2008")
